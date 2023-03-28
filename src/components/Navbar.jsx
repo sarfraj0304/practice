e0043298bec42daf119489cf0ba08a50719b1b43
Mohd Hasan <42648139+alih6051@users.noreply.github.com>
2023-03-28 13:26:01 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,78 @@
-import { Box, Text } from "@chakra-ui/react";
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import CartComponent from './CartComponent';
+import ProductCard from './ProductCard';
 
-const Navbar = () => {
+const Products = () => {
+  const [data, setData] = useState([]);
+  const [cartProducts, setCartProducts] = useState([]);
+
+  const getData = () => {
+    fetch(`http://localhost:${process.env.REACT_APP_JSON_SERVER_PORT}/products`)
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const addToCart = (id, name, price) => {
+    let modData = data.map((item) => {
+      if (item.id === id) {
+        return { ...item, stock: item.stock - 1 };
+      } else {
+        return item;
+      }
+    });
+    setData(modData);
+
+    let added = false;
+    let modCart = cartProducts.map((item) => {
+      if (item.id === id) {
+        added = true;
+        return { ...item, quantity: item.quantity + 1 };
+      } else {
+        return item;
+      }
+    });
+
+    if (added) {
+      setCartProducts(modCart);
+    } else {
+      let item = {
+        id,
+        name,
+        price,
+        quantity: 1,
+      };
+
+      setCartProducts([...cartProducts, item]);
+    }
+  };
+
   return (
-    <Box
-      width={"100%"}
-      p={5}
-      border={"1px solid"}
-      display={"flex"}
-      justifyContent={"space-around"}
-      alignItems={"center"}
-    >
-      <Link to={"/"}>
-        <Text>LOGIN</Text>
-      </Link>
-      <Link to={"/task"}>
-        <Text>TASK</Text>
-      </Link>
-    </Box>
+    <div>
+      {data.length > 0 ? (
+        <div className="dashboard">
+          <h1>Dashboard</h1>
+          <div>
+            <h1>Cart</h1>
+            {/* import cart component here */}
+            <CartComponent cartProducts={cartProducts} />
+          </div>
+          <div data-testid="products-container">
+            {/* map thorugh products */}
+            {data?.map((item) => (
+              <ProductCard key={item.id} {...item} addToCart={addToCart} />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <button data-testid="get-btn" onClick={getData}>
+          Get Products
+        </button>
+      )}
+    </div>
   );
 };
 
-export default Navbar;
+export default Products;
